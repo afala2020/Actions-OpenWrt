fa9d23102212d80401a8d71fd32763adcd800dec
afala2020 <71696868+afala2020@users.noreply.github.com>
2022-09-16 12:51:10 +0800

    Update hiwifi-c526a-part2.sh

--- ipq-40xx/hiwifi-c526a-part2.sh
+++ ipq-40xx/hiwifi-c526a-part2.sh
@@ -7,6 +7,13 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci
 # 修改NAS为网络存储
 sed -i 's/"NAS"/"网络存储"/g' `grep "NAS" -rl ./`
 
+# 修改固件默认时区
+sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
+
+# 修改固件默认名称
+sed -i 's/ImmortalWrt/HIWIFI/g' package/base-files/files/bin/config_generate
+
+
 # 固件架构
 cat >> .config <<EOF
 CONFIG_TARGET_ipq40xx=y
@@ -83,4 +90,4 @@ EOF
 #EOF
 
 # 其它
-sed -i '21d' package/lean/default-settings/files/zzz-default-settings #禁止网络共享分类到NAS
+#sed -i '21d' package/lean/default-settings/files/zzz-default-settings #禁止网络共享分类到NAS

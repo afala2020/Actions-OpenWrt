d863a2a1781d61ba77afa885140674c2de6fa357
afala2020 <71696868+afala2020@users.noreply.github.com>
2022-09-16 12:21:09 +0800

    Update hiwifi-c526a-part1.sh

--- ipq-40xx/hiwifi-c526a-part1.sh
+++ ipq-40xx/hiwifi-c526a-part1.sh
@@ -1,5 +1,2 @@
-# 下载新主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-
-# 删除旧主题
-rm -rf package/lean/luci-theme-argon
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
